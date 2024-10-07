83baf2d6653346e6121ff11866efa885cbc146b8
rehanarroihan <third2014project@gmail.com>
2024-10-07 20:44:01 +0700

    feat: added main organization layout and setting auth rule on the middleware.ts

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const OrganizationIdPage = () => {
+  return (
+    <div className="katon">
+      <OrganizationSwitcher />
+    </div>
+  )
+}
+
+export default OrganizationIdPage
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from "next/server"
 
 const isPublicRoute = createRouteMatcher([
   '/',
@@ -7,7 +8,18 @@ const isPublicRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
+  if (auth().userId && isPublicRoute(request)) {
+    let path = "/select-org"
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`
+    }
+
+    const orgSelection = new URL(path, request.url)
+    return NextResponse.redirect(orgSelection)
+  }
+
+  if (!auth().userId && !isPublicRoute(request)) {
     auth().protect()
   }
 })
